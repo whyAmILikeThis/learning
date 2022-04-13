b6f9cd0481860384aa4a8c7e3cc2b2a3acca013e
whyAmILikeThis <f.smatlo@gmail.com>
2022-04-13 13:34:52 +0200

    fun with classes

---
+++ pyth/classes.py
@@ -0,0 +1,32 @@
+# class Point():
+#     def __init__(self, input1, input2):
+#         self.x = input1
+#         self.y = input2
+
+# p = Point(2, 8)
+# print(p.x)
+# print(p.y)
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+    
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        else:
+            self.passengers.append(name)
+            return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["harry", "hermione", "ron", "ginny"]
+for person in people:
+    if flight.add_passenger(person):
+        print(f"added {person} to flight successfully.")
+    else:
+        print(f"no available seats for {person}") 
