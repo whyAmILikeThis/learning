187bdf59343fa9e6c165ed944f952f7c33dba393
whyAmILikeThis <f.smatlo@gmail.com>
2022-04-20 13:43:12 +0200

    1. project: simple madlib

---
+++ pyth/projects/1_madlib.py
@@ -0,0 +1,17 @@
+# mab libs project
+# super short one
+# ask user for four words
+#   exclamation, adverb, noun, adjective
+
+
+noun = input("enter noun: ")
+occupation = input("enter occupation: ")
+verb = input("enter verb: ")
+adjective = input("enter adjective: ")
+noun2 = input("enter noun: ")
+
+print(f"{noun} from {occupation} {verb} next to a {adjective} {noun2}")
+
+# randomized madlibs
+# write a library with different madlib sentences
+# randomize the input and the you get a random madlib
