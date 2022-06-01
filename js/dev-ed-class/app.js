4d06b52f1ddf7470968233495ce19aa62c74605f
whyAmILikeThis <f.smatlo@gmail.com>
2022-06-01 14:31:39 +0200

    video 1,2 files -  dev ed

---
+++ js/dev-ed-class/app.js
@@ -0,0 +1,17 @@
+// video 1
+// console.log('HIIIII');
+
+//video 2
+// var life = 100;
+// var name = "bleb";
+// console.log(name, life)
+
+// var person = {life: 100, name: 'bleb'}
+// console.log(person.name, person.life)
+
+// var ready = true;
+// if (ready) {
+//     console.log('ready')
+// } else {
+//     console.log('not ready')
+// }
